fb730ae60a6e15f1ecad660e6e51cc021e03e97e
icex2 <djh.icex2@gmail.com>
2020-07-25 14:50:14 +0200

    ddrio-piuio-itg: Expose setting sensor to request on next poll
    Also remove some outdated documentation

--- ddrio/src/piuio/itg/piuio-itg.c
+++ ddrio/src/piuio/itg/piuio-itg.c
@@ -1,9 +1,3 @@
-/**
- * Implementation of the piuio API. This implements a piuio usb driver to
- * communicate with real piuio hardware and also shares a common interface with
- * other "piuio devices" (e.g. keyboard, custom IOs)
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,22 +10,23 @@
 static uint8_t _in_buffer[BUFFER_SIZE];
 static uint8_t _out_buffer[BUFFER_SIZE];
 
-static struct piuio_itg_pad_input _pad_input[PIUIO_ITG_PLAYER_NUM][PIUIO_ITG_SENSOR_GROUP_NUM];
+static struct piuio_itg_pad_input _pad_input[PIUIO_ITG_PLAYER_NUM];
 static struct piuio_itg_button_input _button_input[PIUIO_ITG_PLAYER_NUM];
 static struct piuio_itg_sys_input _sys_input;
+static enum piuio_itg_sensor_group _pad_sensor_group[PIUIO_ITG_PLAYER_NUM];
 static struct piuio_itg_pad_output _pad_output[PIUIO_ITG_PLAYER_NUM];
 static struct piuio_itg_cabinet_output _cabinet_output;
 
-static void _copy_outputs(uint8_t sensor_group)
+static void _copy_outputs()
 {
     memset(_out_buffer, 0, sizeof(_out_buffer));
 
 	/* 00 = sensor 1 (right), 01 = sensor 2 (left), 10 = sensor 3 (down),
        11 = sensor 4 (up) */
     /* Pad 1 sensor group selector */
-    _out_buffer[0] |= sensor_group;
+    _out_buffer[0] |= _pad_sensor_group[0];
     /* Pad 2 sensor group selector */
-    _out_buffer[2] |= sensor_group;
+    _out_buffer[2] |= _pad_sensor_group[1];
 
     /* Set pad lights */
     for (uint8_t i = 0; i < PIUIO_ITG_PLAYER_NUM; i++) {
@@ -81,13 +76,13 @@ static void _copy_outputs(uint8_t sensor_group)
 	// _out_buffer[1] |= ((m_outputStates->m_counter & 0x02) << 3);
 }
 
-static void _copy_inputs(uint8_t sensor_group)
+static void _copy_inputs()
 {
     for (uint8_t i = 0; i < PIUIO_ITG_PLAYER_NUM; i++) {
-        _pad_input[i][sensor_group].up = _in_buffer[i * 2] & (1 << 0);
-        _pad_input[i][sensor_group].down = _in_buffer[i * 2] & (1 << 1);
-        _pad_input[i][sensor_group].left = _in_buffer[i * 2] & (1 << 2);
-        _pad_input[i][sensor_group].right = _in_buffer[i * 2] & (1 << 3);
+        _pad_input[i].up = _in_buffer[i * 2] & (1 << 0);
+        _pad_input[i].down = _in_buffer[i * 2] & (1 << 1);
+        _pad_input[i].left = _in_buffer[i * 2] & (1 << 2);
+        _pad_input[i].right = _in_buffer[i * 2] & (1 << 3);
 
         _button_input[i].start = _in_buffer[i * 2] & (1 << 4);
         _button_input[i].back = _in_buffer[i * 2] & (1 << 5);
@@ -116,41 +111,36 @@ void piuio_itg_close(void)
 
 bool piuio_itg_recv(void)
 {
-    /* cycle all four sensor groups */
-    for (uint8_t i = 0; i < PIUIO_ITG_SENSOR_GROUP_NUM; i++) {
-        _copy_outputs(i);
-
-        if (!piuio_device_write(_out_buffer, sizeof(_out_buffer))) {
-            return false;
-        }
-
-        if (!piuio_device_read(_in_buffer, sizeof(_in_buffer))) {
-            return false;
-        }
-
-        /* Invert pull ups */
-        for (uint8_t j = 0; j < 4; j++) {
-            _in_buffer[j] ^= 0xFF;
-        }
-
-        _copy_inputs(i);
+    if (!piuio_device_read(_in_buffer, sizeof(_in_buffer))) {
+        return false;
     }
+
+    /* Invert pull ups */
+    for (uint8_t j = 0; j < 4; j++) {
+        _in_buffer[j] ^= 0xFF;
+    }
+
+    _copy_inputs();
 
     return true;
 }
 
 bool piuio_itg_send(void)
 {
-    /* Already done in recv */
+    _copy_outputs();
+
+    if (!piuio_device_write(_out_buffer, sizeof(_out_buffer))) {
+        return false;
+    }
+
     return true;
 }
 
 void piuio_itg_get_input_pad(
         enum piuio_itg_player player,
-        enum piuio_itg_sensor_group sensor_group,
         struct piuio_itg_pad_input* input)
 {
-    memcpy(input, &_pad_input[player][sensor_group], sizeof(struct piuio_itg_pad_input));
+    memcpy(input, &_pad_input[player], sizeof(struct piuio_itg_pad_input));
 }
 
 void piuio_itg_get_input_sys(struct piuio_itg_sys_input* input)
@@ -163,6 +153,11 @@ void piuio_itg_get_input_button(
         struct piuio_itg_button_input* input)
 {
     memcpy(input, &_button_input[player], sizeof(struct piuio_itg_button_input));
+}
+
+void piuio_itg_set_output_pad_sensor_group(enum piuio_itg_player player, enum piuio_itg_sensor_group sensor_group)
+{
+    _pad_sensor_group[player] = sensor_group;
 }
 
 void piuio_itg_set_output_pad(
--- ddrio/src/piuio/itg/piuio-itg.h
+++ ddrio/src/piuio/itg/piuio-itg.h
@@ -1,7 +1,3 @@
-/**
- * Common API to use for any devices implementing (MK6) piuio controls
- * (e.g. keyboard emulation, custom IOs)
- */
 #pragma once
 
 #include <stdbool.h>
@@ -61,68 +57,22 @@ struct piuio_itg_cabinet_output {
     bool halo_l1;
 };
 
-/**
- * Open the piuio device
- *
- * Open any file handles, spawn threads and/or initialize your device or emulation code to be ready to accept further
- * commands.
- *
- * @return True if opening/initializing your implementation was sucessful, false on failure
- */
 bool piuio_itg_open(void);
 
-/**
- * Close and cleanup your device, shut down threads, free resources etc
- */
 void piuio_itg_close(void);
 
-/**
- * Get _ALL_ readable data from your device in this single recv call. If you have to address single sensores like the
- * real piuio, you have to do this on a single call to this function.
- *
- * @return True on success, false on error
- */
 bool piuio_itg_recv(void);
 
-/**
- * Send any output data (e.g. lights) to your device
- *
- * @return True on success, false on error
- */
 bool piuio_itg_send(void);
 
-/**
- * Get buffered pad input data
- *
- * @param player Player id (0, 1)
- * @param sensor_group Sensor group states to get
- * @param inputs Pointer to buffer to copy inputs of the selected sensor group to. Every time this is invoked, this
- *               buffer is nulled. If you do not write any data to this, all inputs remain un-triggered.
- */
-void piuio_itg_get_input_pad(enum piuio_itg_player player, enum piuio_itg_sensor_group sensor_group,
-        struct piuio_itg_pad_input* input);
+void piuio_itg_get_input_pad(enum piuio_itg_player player, struct piuio_itg_pad_input* input);
 
 void piuio_itg_get_input_button(enum piuio_itg_player player, struct piuio_itg_button_input* input);
 
-/**
- * Get buffered system (test, service, clear, coin) inputs
- *
- * @param inputs Pointer to buffer to copy inputs to. Every time this is invoked, this buffer is nulled. If you do not
- *               write any data to this, all inputs remain un-triggered.
- */
 void piuio_itg_get_input_sys(struct piuio_itg_sys_input* inputs);
 
-/**
- * Set pad light outputs (i.e. buffer them)
- *
- * @param player Player id (0, 1)
- * @param outputs Pointer to output data to copy to your private buffers.
- */
+void piuio_itg_set_output_pad_sensor_group(enum piuio_itg_player player, enum piuio_itg_sensor_group sensor_group);
+
 void piuio_itg_set_output_pad(enum piuio_itg_player player, const struct piuio_itg_pad_output* output);
 
-/**
- * Set cabinet light outputs (i.e. buffer them)
- *
- * @param outputs Pointer to output data to copy to your private buffers.
- */
 void piuio_itg_set_output_cabinet(const struct piuio_itg_cabinet_output* output);
